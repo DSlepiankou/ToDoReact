8ecbb088245af93260e443e6d31744b85ed2ff25
DSlepiankou <Slepenkov95@mail.ru>
2023-06-20 20:07:02 +0300

    change config file

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,17 +1,12 @@
 const hq = require('alias-hq')
 
 module.exports = {
-  transform: {
-    "\\.[jt]sx?$":  [ 'esbuild-jest', { 
-        loaders: {
-          '.spec.js': 'jsx',
-          '.js': 'jsx'
-        }
-      }
-    ]
-  },
-  /// This will resolve any tsconfig.compilerOptions.paths
-  moduleNameMapper: hq.get('jest'),
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/types/' ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    testEnvironment: 'jsdom',
+    transform: {
+        '\\.[jt]sx?$': 'esbuild-jest',
+        //   /// This will resolve any tsconfig.compilerOptions.paths
+        //   moduleNameMapper: hq.get('jest'),
+        //   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/types/' ],
+        //   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    },
 }
